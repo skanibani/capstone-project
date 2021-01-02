bf6e713c90c241fb76beb89f424967a0a3348e9c
Skander <skander@bibani.nl>
2021-01-02 20:23:04 +0100

    Add DrawerLayout and Views

--- app/src/main/java/com/example/sherlock/MainActivity.kt
+++ app/src/main/java/com/example/sherlock/MainActivity.kt
@@ -6,17 +6,19 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setSupportActionBar(findViewById(R.id.topAppBar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        findViewById<MaterialToolbar>(R.id.topAppBar).setNavigationOnClickListener {
+            findViewById<DrawerLayout>(R.id.drawerLayout).open()
         }
     }
 

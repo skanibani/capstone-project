d81fcf4e94df734ffed820513878b2fe7c779755
Skander <skander@bibani.nl>
2021-01-03 12:31:16 +0100

    Add Room database

---
+++ app/src/main/java/com/example/sherlock/database/ItemDatabase.kt
@@ -0,0 +1,37 @@
+package com.example.sherlock.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.sherlock.model.Item
+
+@Database(entities = [Item::class], version = 1, exportSchema = false)
+abstract class ItemDatabase : RoomDatabase() {
+
+    abstract val itemDao: ItemDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: ItemDatabase? = null
+
+        fun getInstance(context: Context): ItemDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            ItemDatabase::class.java,
+                            "item_database"
+                    )
+                            .fallbackToDestructiveMigration()
+                            .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
